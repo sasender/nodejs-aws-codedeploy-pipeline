e7b6c015a7b7070f4b578ada08edf61f081129fa
sasender <78895362+sasender@users.noreply.github.com>
2022-12-23 08:55:04 +0530

    adding

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
-const port = 3000;
+const port = 80;
 
-const version = '3';
+const version = '4';
 
 app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:green;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
+                        <h1 style="color:green;text-align: center;margin-top: 100px;"> [Version ${version}]: sasender!!! reddy!</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
