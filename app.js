67c524bf656d21f8d029476696f8b3d4d1e28abd
sasender <78895362+sasender@users.noreply.github.com>
2023-04-15 12:52:04 +0530

    Adding versions5

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 80;
 
-const version = '4';
+const version = '5';
 
 app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:green;text-align: center;margin-top: 100px;"> [Version ${version}]: sasender!!! reddy!</h1>
+                        <h1 style="color:green;text-align: center;margin-top: 100px;"> [Version ${version}]: Sasender Reddy!</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
